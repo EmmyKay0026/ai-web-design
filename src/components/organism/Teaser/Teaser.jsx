aa512b322f3e84543b6ca5dc6229c9a5df0db157
EmmyKay0026 <kadiriemmanueloshio0026@gmail.com>
2023-08-03 15:26:38 +0100

    Teaser text updated

--- src/components/organism/Teaser/Teaser.jsx
+++ src/components/organism/Teaser/Teaser.jsx
@@ -6,9 +6,9 @@ export const Teaser = () => {
   return (
     <div className="teaser-cont">
       <h4>
-        Artificial intelligence is not here to take your jobs, it is here to
-        make your job easier. Take advantage of AI to make money, save money and
-        stay ahead of your competitors.
+        The evolution of AI has just begun, and if you don't stay updated, you
+        will be left behind. Take advantage of this evolution to make more
+        money, save more and stay ahead of your competitiors
       </h4>
       {/* <Button className="teaser-btn btn" children="Enroll Now" /> */}
       <div className="teaser-img">
